f47223912056e162ec3115cc27916e556146f8b2
Christopher Koning <chris@autocloud.dev>
2022-04-03 15:43:12 -0700

    fix: add stub source and test for pipeline testing

---
+++ test/stub.test.ts
@@ -0,0 +1,9 @@
+describe('Stub test: ', () => {
+  const result = true
+
+  describe('Always Pass', () => {
+    test('should return a truthy value ', () => {
+      expect(result).toBeTruthy()
+    })
+  })
+})
